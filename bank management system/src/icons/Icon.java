189398ac898bf00d91fcecf3d9ef95460b3d0cba
ParagKgiri <Paragkgiri18@gmail.com>
2023-12-27 04:10:07 +0530

    Initial commit

---
+++ bank management system/src/icons/Icon.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Other/File.java to edit this template
+ */
+package icons;
+
+/**
+ *
+ * @author 1234
+ */
+public class Icon {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String args[]) {
+        // TODO code application logic here
+    }
+}
